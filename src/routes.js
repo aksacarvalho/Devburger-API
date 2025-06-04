5eb3c631bdc1a1d6051bd63bfb30da901af49a42
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-06-04 16:43:35 -0300

    criando migration de produtos/criando controller de produtos/upload de imagens com multer

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './App/controllers/UserController';
 import SessionController from './App/controllers/SessionController';
+import productController from './App/controllers/productController';
+
+
+
 
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.post('/products', upload.single('file'), productController.store);
+
+
+
 
 
 export default routes;
