51b063a51f4f2f455541dbebc5b05dc842d0154d
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-06-13 15:45:37 -0300

    criando e excluindo campos de uma tabela

---
+++ src/database/migrations/20250613175214-remove-categories-column.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { default: Category } = require('../../App/models/Category');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.removeColumn('products', 'category');
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.addColumn('products', 'category', {
+            type: Sequelize.STRING,
+            allowNull: true,
+           });
+      },
+};
