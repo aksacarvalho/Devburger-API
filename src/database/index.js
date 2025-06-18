0a77e8725dd91d13925df9fb2a6c4a66433e08d8
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-06-18 15:07:04 -0300

    criando conexão com MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import configDatabase from '../config/database.js';
 
@@ -12,6 +13,7 @@ const models = [User, Product, Category];
 class Database {
    constructor() {
     this.init();
+    this.mongo();
   
 }
 
@@ -24,7 +26,12 @@ init() {
         (model) => model.associate && model.associate(this.connection.models),
       );
   }
-
+ 
+   mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburger',
+    );
+  }
 }
 
 export default new Database();
